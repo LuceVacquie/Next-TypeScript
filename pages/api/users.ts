113dda7465b66012a0b0bb0e7ec0126230888f6c
Lucile Vacquie <lucilevacquie@Luciles-Mac-mini.local>
2021-07-21 12:35:09 +0100

    link the APIs

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,17 +1,14 @@
 import React from 'react'
 import { NextApiRequest, NextApiResponse } from 'next'
-import sqlite from 'sqlite'
-// import sqlite3 from 'sqlite3'
+import {open} from 'sqlite'
+import sqlite3 from 'sqlite3'
 
-// async function openDb() {
-//     return sqlite.open({
-//         filename: '../../database.db',
-//         driver: sqlite3.Database
-//     });
-// }
 
 const getUsers = async (req: NextApiRequest, res: NextApiResponse) => {
-    const db = await sqlite.open('./mydb.sqlite')
+    const db = await open({
+        filename: './database.db',
+        driver: sqlite3.Database
+    })
 
     const users = await db.all('select * from user')
     
--- pages/api/vehicules.ts
+++ pages/api/vehicules.ts
@@ -1,12 +1,18 @@
 import React from 'react'
 import { NextApiRequest, NextApiResponse } from 'next'
+import {open} from 'sqlite'
+import sqlite3 from 'sqlite3'
 
 
-const getAllVehicules = (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method !== 'GET'){
-        res.status(500).json({message: 'Sorry we only accept GET request'})
-    }
-    res.json({hello: 'world', method: req.method})
+const getVehicules = async (req: NextApiRequest, res: NextApiResponse) => {
+    const db = await open({
+        filename: './database.db',
+        driver: sqlite3.Database
+    })
+
+    const vehicules = await db.all('select * from vehicule')
+    
+    res.json(vehicules)
 }
 
-export default getAllVehicules;
+export default getVehicules;

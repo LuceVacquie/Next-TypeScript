5f23abaa28db11c2dda4237b90b27df619623e35
Lucile Vacquie <lucilevacquie@Luciles-Mac-mini.local>
2021-07-23 10:49:38 +0100

    removed login function from login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,33 +1,33 @@
 import React, { useRef, useState } from 'react'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import {useAuthContext} from '../AuthProvider'
 
 const Login = () => {
 
-    const { setIsLoggedIn }:any = useAuthContext()
+    const { handleLogin, emailRef, passwordRef }:any = useAuthContext()
 
-    const emailRef = useRef<HTMLInputElement>(null)
-    const passwordRef = useRef<HTMLInputElement>(null)
+    // const emailRef = useRef<HTMLInputElement>(null)
+    // const passwordRef = useRef<HTMLInputElement>(null)
 
-    const router = useRouter()
+    // const router = useRouter()
 
-    const handleLogin = async () => {
-        const response = await fetch('http://localhost:3000/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify({
-                email: emailRef.current?.value,
-                password: passwordRef.current?.value
-            })
-        })
-        const json = await response.json()
+    // const handleLogin = async () => {
+    //     const response = await fetch('http://localhost:3000/api/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type' : 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             email: emailRef.current?.value,
+    //             password: passwordRef.current?.value
+    //         })
+    //     })
+    //     const json = await response.json()
 
-        setIsLoggedIn(true)
+    //     setIsLoggedIn(true)
         
-        router.push('/')
-    }
+    //     router.push('/')
+    // }
 
     return(
         <div>
